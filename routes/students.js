a1660c5fd8af4f6bc1b0bd64bb255935c1d58207
Ekta <dudh0004@algonquinlive.com>
2021-03-04 16:49:46 -0500

    PATCH route of student

--- routes/students.js
+++ routes/students.js
@@ -19,6 +19,37 @@ router.get('/:id', sanitizeBody, async (req, res) => {
     }
 })
 
+router.post('/', sanitizeBody, async (req, res) => {
+    try {
+        const newStudent = new Student(req.sanitizedBody)
+        await newStudent.save()
+        res.send({data: newStudent})
+        } catch (err) {
+        sendResourceNotFound(err)
+        }
+    })
+
+router.patch('/:id', sanitizeBody, async (req, res) => {
+    const {_id, id, ...otherAttributes} = req.sanitizedBody
+    try { 
+    const student = await Student.findByIdAndUpdate(
+        req.params.id, 
+        {_id: req.params.id, ...otherAttributes}, 
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+    if (!student) {
+        throw new Error('Resource not found')
+    }
+    res.send({data: student})
+    } catch (err) {
+        sendResourceNotFound(req, res)
+    }
+})
+
+
 function sendResourceNotFound(req, res) {
     res.status(404).send({
         errors: [
@@ -30,16 +61,5 @@ function sendResourceNotFound(req, res) {
         ]
     })
 }
-
-router.post('/', sanitizeBody, async (req, res) => {
-    try {
-        const newStudent = new Student(req.sanitizedBody)
-        await newStudent.save()
-        res.send({data: newStudent})
-        } catch (err) {
-        sendResourceNotFound(err)
-        }
-    })
-
 
 module.exports = router
